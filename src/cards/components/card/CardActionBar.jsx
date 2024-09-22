9e829084dfa099132e4692536e937dedc9a8f038
mor3162 <more3031996@gmail.com>
2024-09-22 10:11:24 +0300

    fix bug

--- src/cards/components/card/CardActionBar.jsx
+++ src/cards/components/card/CardActionBar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import DeleteIcon from "@mui/icons-material/Delete";
 import ModeEditIcon from "@mui/icons-material/ModeEdit";
 import CallIcon from "@mui/icons-material/Call";
@@ -7,6 +7,7 @@ import { Box, IconButton, CardActions } from "@mui/material";
 import { useCurrentUser } from "../../../users/providers/UserProvider";
 import { Link, useNavigate } from "react-router-dom";
 import ROUTES from "../../../routes/routesModel";
+import { useState } from "react";
 
 export default function CardActionBar({
   cardId,
@@ -17,9 +18,17 @@ export default function CardActionBar({
   handleLike,
   phone,
 }) {
+
   const { user } = useCurrentUser();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (user) setLoading(false);
+  }, [user]);
+
 
   return (
+    !loading &&
     <CardActions sx={{ justifyContent: "space-between" }}>
       <Box>
         {
@@ -46,3 +55,4 @@ export default function CardActionBar({
     </CardActions>
   );
 }
+
